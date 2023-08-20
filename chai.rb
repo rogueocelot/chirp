8a1b50233cd0da8cb548efff0ef30e29e979370f
Eric <epasho@yahoo.com>
2023-08-19 22:24:55 -0400

    added functionality

--- chai.rb
+++ chai.rb
@@ -1,1 +1,44 @@
 #CHatgpt Answer Interface
+#require './discordrb'
+require 'discordrb'
+require 'dotenv'
+require 'openai'
+
+#load enviroment variables from .env file
+Dotenv.load
+#load discord bot token and openai access token
+discordtoken = ENV['discordtoken']
+openaitoken = ENV['openaitoken']
+bot = Discordrb::Bot.new token: discordtoken
+client = OpenAI::Client.new(access_token: openaitoken)
+
+bot.message(start_with: 'chai') do |event|
+
+  #remove the first (triggering) word (-1 is last element in ruby)
+  question = "#{event.message}"
+  question = question.split(' ')[1..-1].join(' ')
+  #print question
+
+  #chatgpt api call
+  response = client.chat(
+    parameters: {
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: question}],
+      temperature: 0.5,
+    }
+  )
+  #get chatgpt message
+  message = response.dig("choices", 0, "message", "content")
+
+  if message.length > 1990
+    message = message[0..1970] + "..."
+  end
+
+  #print the triggering message
+  #print event.message
+
+  #send response back to discord
+  event.respond message
+end
+
+bot.run
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 # chai-ruby
 
-CHatgpt Answer Interface
+CHatgpt Answer Interface  
 This program creates a discord bot to relay questions and answers from ChatGPT.
